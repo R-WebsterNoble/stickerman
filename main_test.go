bed433ebef5e80991ec1e20da9549f8dd8b94045
R-WebsterNoble <R.WebsterNoble@gmail.com>
2018-06-17 14:38:40 +0100

    Brought tests into main package

--- main_test.go
+++ main_test.go
@@ -1,8 +1,7 @@
-package main_test
+package main
 
 import (
 	"testing"
-	"github.com/R-WebsterNoble/stickerman"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/stretchr/testify/assert"
 	"os"
@@ -10,7 +9,7 @@ import (
 )
 
 func TestMain(m *testing.M) {
-	defer main.Shutdown()
+	defer Shutdown()
 
 	testDbName := "stampstest"
 	adminDb := setupTestDB(testDbName)
@@ -34,13 +33,13 @@ func setupTestDB(dbName string) (adminDb *sql.DB) {
 	_, err = adminDb.Exec("CREATE DATABASE " + dbName)
 	checkErr(err)
 
-	main.SetupDB("schema.sql")
+	SetupDB("schema.sql")
 
 	return adminDb
 }
 
 func tearDownDB(adminDb *sql.DB, dbName string) {
-	main.CloseDb()
+	CloseDb()
 	defer adminDb.Close()
 
 	_, err := adminDb.Exec("DROP DATABASE IF EXISTS " + dbName)
@@ -51,8 +50,7 @@ func tearDownDB(adminDb *sql.DB, dbName string) {
 func TestHandler_HandlesUnknownMessage(t *testing.T) {
 	request := events.APIGatewayProxyRequest{Body: "{\"update_id\":457211654,\"edited_message\":{\"message_id\":64,\"from\":{\"id\":12345,\"is_bot\":false,\"first_name\":\"user\",\"username\":\"user\",\"language_code\":\"en-GB\"},\"chat\":{\"id\":12345,\"first_name\":\"user\",\"username\":\"user\",\"type\":\"private\"},\"date\":1524691085,\"edit_date\":1524693406,\"text\":\"hig\"}}"}
 
-
-	response, err := main.Handler(request)
+	response, err := Handler(request)
 
 	assert.IsType(t, err, nil)
 	assert.Equal(t, "unable to process request: neither message or update found", response.Body)
@@ -61,7 +59,7 @@ func TestHandler_HandlesUnknownMessage(t *testing.T) {
 func TestHandler_HandlesInvalidJson(t *testing.T) {
 	request := events.APIGatewayProxyRequest{Body: "!"}
 
-	response, err := main.Handler(request)
+	response, err := Handler(request)
 
 	assert.IsType(t, err, nil)
 	assert.Equal(t, "error while Unmarshaling", response.Body)
@@ -70,7 +68,7 @@ func TestHandler_HandlesInvalidJson(t *testing.T) {
 func TestHandler_HandlesMessage(t *testing.T) {
 	request := events.APIGatewayProxyRequest{Body: "{\"update_id\":457211650,\"message\":{\"message_id\":65,\"from\":{\"id\":12345,\"is_bot\":false,\"first_name\":\"user\",\"username\":\"user\",\"language_code\":\"en-GB\"},\"chat\":{\"id\":12345,\"first_name\":\"user\",\"username\":\"user\",\"type\":\"private\"},\"date\":1524692383,\"text\":\"/start\",\"entities\":[{\"offset\":0,\"length\":6,\"type\":\"bot_command\"}]}}"}
 
-	response, err := main.Handler(request)
+	response, err := Handler(request)
 
 	assert.IsType(t, err, nil)
 	expected := "{\"method\":\"sendMessage\",\"chat_id\":12345,\"text\":\"This Bot is designed to help you find stickers.\\n\\nUsage:\\nTo search for a stickers in any chat type: @DevStampsBot followed by your search keywords.\\n\\nTo add new sticker and keywords to the bot, first send the sticker to this chat.\"}"
@@ -80,7 +78,7 @@ func TestHandler_HandlesMessage(t *testing.T) {
 func TestHandler_HandlesSticker(t *testing.T) {
 	request := events.APIGatewayProxyRequest{Body: "{\"update_id\":457211708,\"message\":{\"message_id\":315,\"from\":{\"id\":12345,\"is_bot\":false,\"first_name\":\"user\",\"username\":\"user\",\"language_code\":\"en-GB\"},\"chat\":{\"id\":12345,\"first_name\":\"user\",\"username\":\"user\",\"type\":\"private\"},\"date\":1524775382,\"sticker\":{\"width\":512,\"height\":512,\"emoji\":\"👉\",\"set_name\":\"Feroxdoon2\",\"thumb\":{\"file_id\":\"AAQBABOqNQMwAAQ78UrarWIt0iRYAAIC\",\"file_size\":4670,\"width\":128,\"height\":128},\"file_id\":\"CAADAQADKgAD5_bHDFnhkQhE_myDAg\",\"file_size\":24458}}}"}
 
-	response, err := main.Handler(request)
+	response, err := Handler(request)
 
 	assert.IsType(t, err, nil)
 	expected := "{\"method\":\"sendMessage\",\"chat_id\":12345,\"text\":\"That's a nice sticker. Send me some keywords and I'll add them to it.\"}"
@@ -90,7 +88,7 @@ func TestHandler_HandlesSticker(t *testing.T) {
 func TestHandler_HandlesStickerReply(t *testing.T) {
 	request := events.APIGatewayProxyRequest{Body: "{\"message\":{\"message_id\":359,\"from\":{\"id\":12345,\"is_bot\":false,\"first_name\":\"user\",\"username\":\"user\",\"language_code\":\"en-GB\"},\"chat\":{\"id\":12345,\"first_name\":\"user\",\"username\":\"user\",\"type\":\"private\"},\"date\":1525458701,\"reply_to_message\":{\"message_id\":321,\"from\":{\"id\":12345,\"is_bot\":false,\"first_name\":\"user\",\"username\":\"user\",\"language_code\":\"en-GB\"},\"chat\":{\"id\":12345,\"first_name\":\"user\",\"username\":\"user\",\"type\":\"private\"},\"date\":1524777329,\"sticker\":{\"width\":512,\"height\":512,\"emoji\":\"👉\",\"set_name\":\"Feroxdoon2\",\"thumb\":{\"file_id\":\"AAQBABOqNQMwAAQ78UrarWIt0iRYAAIC\",\"file_size\":4670,\"width\":128,\"height\":128},\"file_id\":\"CAADAQADKgAD5_bHDFnhkQhE_myDAg\",\"file_size\":24458}},\"text\":\"keyword\"}}"}
 
-	response, err := main.Handler(request)
+	response, err := Handler(request)
 
 	assert.IsType(t, err, nil)
 	expected := "{\"method\":\"sendMessage\",\"chat_id\":12345,\"text\":\"Added 0 keyword(s).\"}"
@@ -101,7 +99,7 @@ func TestHandler_HandlesStickerReplyWithMultipleKeywords(t *testing.T) {
 	t.SkipNow() // need to setup test db fixtures for this to work more than once
 	request := events.APIGatewayProxyRequest{Body: "{\"message\":{\"message_id\":359,\"from\":{\"id\":12345,\"is_bot\":false,\"first_name\":\"user\",\"username\":\"user\",\"language_code\":\"en-GB\"},\"chat\":{\"id\":12345,\"first_name\":\"user\",\"username\":\"user\",\"type\":\"private\"},\"date\":1525458701,\"reply_to_message\":{\"message_id\":321,\"from\":{\"id\":12345,\"is_bot\":false,\"first_name\":\"user\",\"username\":\"user\",\"language_code\":\"en-GB\"},\"chat\":{\"id\":12345,\"first_name\":\"user\",\"username\":\"user\",\"type\":\"private\"},\"date\":1524777329,\"sticker\":{\"width\":512,\"height\":512,\"emoji\":\"👉\",\"set_name\":\"Feroxdoon2\",\"thumb\":{\"file_id\":\"AAQBABOqNQMwAAQ78UrarWIt0iRYAAIC\",\"file_size\":4670,\"width\":128,\"height\":128},\"file_id\":\"CAADAQADKgAD5_bHDFnhkQhE_myDAg\",\"file_size\":24458}},\"text\":\"keyword1 keyword2\"}}"}
 
-	response, err := main.Handler(request)
+	response, err := Handler(request)
 
 	assert.IsType(t, err, nil)
 	expected := "{\"method\":\"sendMessage\",\"chat_id\":12345,\"text\":\"Added 2 keyword(s).\"}"
@@ -111,7 +109,7 @@ func TestHandler_HandlesStickerReplyWithMultipleKeywords(t *testing.T) {
 func TestHandler_HandlesEmptyInlineQuery(t *testing.T) {
 	request := events.APIGatewayProxyRequest{Body: "{\"update_id\":457211742,\"inline_query\":{\"id\":\"913797545109391540\",\"from\":{\"id\":12345,\"is_bot\":false,\"first_name\":\"user\",\"username\":\"user\",\"language_code\":\"en-GB\"},\"query\":\"\",\"offset\":\"\"}}"}
 
-	response, err := main.Handler(request)
+	response, err := Handler(request)
 
 	assert.IsType(t, err, nil)
 	expected := "{\"method\":\"answerInlineQuery\",\"inline_query_id\":\"913797545109391540\",\"results\":[]}"
@@ -121,7 +119,7 @@ func TestHandler_HandlesEmptyInlineQuery(t *testing.T) {
 func TestHandler_HandlesInlineQuery(t *testing.T) {
 	request := events.APIGatewayProxyRequest{Body: "{\"update_id\":457211742,\"inline_query\":{\"id\":\"913797545109391540\",\"from\":{\"id\":12345,\"is_bot\":false,\"first_name\":\"user\",\"username\":\"user\",\"language_code\":\"en-GB\"},\"query\":\"CAADAgADAQMAApzW5woyIbXtrGvnsAI\",\"offset\":\"\"}}"}
 
-	response, err := main.Handler(request)
+	response, err := Handler(request)
 
 	assert.IsType(t, err, nil)
 	expected := "{\"method\":\"answerInlineQuery\",\"inline_query_id\":\"913797545109391540\",\"results\":[{\"type\":\"sticker\",\"id\":\"0\",\"sticker_file_id\":\"CAADAgADAQMAApzW5woyIbXtrGvnsAI\"}]}"
@@ -131,7 +129,7 @@ func TestHandler_HandlesInlineQuery(t *testing.T) {
 func TestHandler_HandlesInlineQueryWithSQLI(t *testing.T) {
 	request := events.APIGatewayProxyRequest{Body: "{\"update_id\":457211742,\"inline_query\":{\"id\":\"913797545109391540\",\"from\":{\"id\":12345,\"is_bot\":false,\"first_name\":\"user\",\"username\":\"user\",\"language_code\":\"en-GB\"},\"query\":\" '''\",\"offset\":\"\"}}"}
 
-	response, err := main.Handler(request)
+	response, err := Handler(request)
 
 	assert.IsType(t, err, nil)
 	expected := "{\"method\":\"answerInlineQuery\",\"inline_query_id\":\"913797545109391540\",\"results\":[{\"type\":\"sticker\",\"id\":\"0\",\"sticker_file_id\":\"CAADAQADKgAD5_bHDFnhkQhE_myDAg\"}]}"
@@ -141,7 +139,7 @@ func TestHandler_HandlesInlineQueryWithSQLI(t *testing.T) {
 func TestHandler_HandlesInlineQueryWithMultipleResults(t *testing.T) {
 	request := events.APIGatewayProxyRequest{Body: "{\"update_id\":457211742,\"inline_query\":{\"id\":\"913797545109391540\",\"from\":{\"id\":12345,\"is_bot\":false,\"first_name\":\"user\",\"username\":\"user\",\"language_code\":\"en-GB\"},\"query\":\"sB0Umf2MBsk\",\"offset\":\"\"}}"}
 
-	response, err := main.Handler(request)
+	response, err := Handler(request)
 
 	assert.IsType(t, err, nil)
 	expected := "{\"method\":\"answerInlineQuery\",\"inline_query_id\":\"913797545109391540\",\"results\":[{\"type\":\"sticker\",\"id\":\"0\",\"sticker_file_id\":\"CAADAgADFQAD2EMzEnvdd9kfrCGwAg\"},{\"type\":\"sticker\",\"id\":\"1\",\"sticker_file_id\":\"CAADAgADFgAD2EMzEqA6t2tUdswBAg\"}]}"
@@ -151,7 +149,7 @@ func TestHandler_HandlesInlineQueryWithMultipleResults(t *testing.T) {
 func TestHandler_HandlesInlineQueryWithMultipleKeywords(t *testing.T) {
 	request := events.APIGatewayProxyRequest{Body: "{\"update_id\":457211742,\"inline_query\":{\"id\":\"913797545109391540\",\"from\":{\"id\":12345,\"is_bot\":false,\"first_name\":\"user\",\"username\":\"user\",\"language_code\":\"en-GB\"},\"query\":\"degWs89raGY vjmvodk8LG8\",\"offset\":\"\"}}"}
 
-	response, err := main.Handler(request)
+	response, err := Handler(request)
 
 	assert.IsType(t, err, nil)
 	expected := "{\"method\":\"answerInlineQuery\",\"inline_query_id\":\"913797545109391540\",\"results\":[{\"type\":\"sticker\",\"id\":\"0\",\"sticker_file_id\":\"CAADAwADWwEAAm9iOwdJbHljxEZDHgI\"},{\"type\":\"sticker\",\"id\":\"1\",\"sticker_file_id\":\"CAADAwADgwEAAm9iOweRXewEFMcJ2gI\"}]}"
@@ -161,7 +159,7 @@ func TestHandler_HandlesInlineQueryWithMultipleKeywords(t *testing.T) {
 func TestHandler_HandlesInlineQueryMatchesAllKeywords(t *testing.T) {
 	request := events.APIGatewayProxyRequest{Body: "{\"update_id\":457211742,\"inline_query\":{\"id\":\"913797545109391540\",\"from\":{\"id\":12345,\"is_bot\":false,\"first_name\":\"user\",\"username\":\"user\",\"language_code\":\"en-GB\"},\"query\":\"guhYMplLkEtJ3Q l27rGJHCmOqomg\",\"offset\":\"\"}}"}
 
-	response, err := main.Handler(request)
+	response, err := Handler(request)
 
 	assert.IsType(t, err, nil)
 	expected := "{\"method\":\"answerInlineQuery\",\"inline_query_id\":\"913797545109391540\",\"results\":[{\"type\":\"sticker\",\"id\":\"0\",\"sticker_file_id\":\"CAADAgAD8wIAApzW5wrgLgRxhQ_BAgI\"},{\"type\":\"sticker\",\"id\":\"1\",\"sticker_file_id\":\"CAADAgADMAIAAs-71A59r1FSPKQrowI\"}]}"
@@ -171,7 +169,7 @@ func TestHandler_HandlesInlineQueryMatchesAllKeywords(t *testing.T) {
 func TestHandler_HandlesInlineQueryMatchesAllKeywordsWithCompletion(t *testing.T) {
 	request := events.APIGatewayProxyRequest{Body: "{\"update_id\":457211742,\"inline_query\":{\"id\":\"913797545109391540\",\"from\":{\"id\":12345,\"is_bot\":false,\"first_name\":\"user\",\"username\":\"user\",\"language_code\":\"en-GB\"},\"query\":\"CAADAgADAQMAApzW5woyIbX\",\"offset\":\"\"}}"}
 
-	response, err := main.Handler(request)
+	response, err := Handler(request)
 
 	assert.IsType(t, err, nil)
 	expected := "{\"method\":\"answerInlineQuery\",\"inline_query_id\":\"913797545109391540\",\"results\":[{\"type\":\"sticker\",\"id\":\"0\",\"sticker_file_id\":\"CAADAgADAQMAApzW5woyIbXtrGvnsAI\"}]}"
@@ -181,7 +179,7 @@ func TestHandler_HandlesInlineQueryMatchesAllKeywordsWithCompletion(t *testing.T
 func TestHandler_HandlesInlineQueryEscapesWildcards(t *testing.T) {
 	request := events.APIGatewayProxyRequest{Body: "{\"update_id\":457211742,\"inline_query\":{\"id\":\"913797545109391540\",\"from\":{\"id\":12345,\"is_bot\":false,\"first_name\":\"user\",\"username\":\"user\",\"language_code\":\"en-GB\"},\"query\":\"Per\\\\_\\\\%\\\\\\\\x\",\"offset\":\"\"}}"}
 
-	response, err := main.Handler(request)
+	response, err := Handler(request)
 
 	assert.IsType(t, err, nil)
 	expected := "{\"method\":\"answerInlineQuery\",\"inline_query_id\":\"913797545109391540\",\"results\":[{\"type\":\"sticker\",\"id\":\"0\",\"sticker_file_id\":\"CAADAgADCwMAApzW5wrRyWd0_dJz5QI\"}]}"
@@ -189,14 +187,14 @@ func TestHandler_HandlesInlineQueryEscapesWildcards(t *testing.T) {
 }
 
 func TestHandler_GetUserState_GetsUserState(t *testing.T) {
-	resultStickerId, resultMode := main.GetUserState(1)
+	resultStickerId, resultMode := GetUserState(1)
 
 	assert.Equal(t, "test", resultStickerId)
 	assert.Equal(t, "add", resultMode)
 }
 
 func TestHandler_GetUserState_GetsInvalidUserState(t *testing.T) {
-	resultStickerId, resultMode := main.GetUserState(-1)
+	resultStickerId, resultMode := GetUserState(-1)
 
 	assert.Equal(t, "", resultStickerId)
 	assert.Equal(t, "", resultMode)
@@ -204,7 +202,7 @@ func TestHandler_GetUserState_GetsInvalidUserState(t *testing.T) {
 
 func TestHandler_SetUserState_SetsUserState(t *testing.T) {
 
-	resultMode := main.SetUserStickerAndGetMode(1, "test")
+	resultMode := SetUserStickerAndGetMode(1, "test")
 
 	assert.Equal(t, "add", resultMode)
 }
@@ -212,7 +210,7 @@ func TestHandler_SetUserState_SetsUserState(t *testing.T) {
 func TestHandler_HandlesKeywordMessageWithNoState(t *testing.T) {
 	request := events.APIGatewayProxyRequest{Body: "{\"message\":{\"message_id\":900,\"from\":{\"id\":0,\"is_bot\":false,\"first_name\":\"blah\",\"username\":\"blah\",\"language_code\":\"en-GB\"},\"chat\":{\"id\":0,\"first_name\":\"user\",\"username\":\"user\",\"type\":\"private\"},\"date\":1527633135,\"text\":\"hi\"}}"}
 
-	response, err := main.Handler(request)
+	response, err := Handler(request)
 
 	assert.IsType(t, err, nil)
 	expected := "{\"method\":\"sendMessage\",\"chat_id\":0,\"text\":\"Send a sticker to me then I'll be able to add searchable keywords to it.\"}"
@@ -222,7 +220,7 @@ func TestHandler_HandlesKeywordMessageWithNoState(t *testing.T) {
 func TestHandler_HandlesKeywordState(t *testing.T) {
 	request := events.APIGatewayProxyRequest{Body: "{\"message\":{\"message_id\":900,\"from\":{\"id\":12345,\"is_bot\":false,\"first_name\":\"blah\",\"username\":\"blah\",\"language_code\":\"en-GB\"},\"chat\":{\"id\":12345,\"first_name\":\"user\",\"username\":\"user\",\"type\":\"private\"},\"date\":1527633135,\"text\":\"hi\"}}"}
 
-	response, err := main.Handler(request)
+	response, err := Handler(request)
 
 	assert.IsType(t, err, nil)
 	expected := "{\"method\":\"sendMessage\",\"chat_id\":12345,\"text\":\"Added 0 keyword(s).\"}"
@@ -232,7 +230,7 @@ func TestHandler_HandlesKeywordState(t *testing.T) {
 func TestHandler_HandlesKeywordMessageWithRemove(t *testing.T) {
 	request := events.APIGatewayProxyRequest{Body: "{\"message\":{\"message_id\":900,\"from\":{\"id\":12345,\"is_bot\":false,\"first_name\":\"blah\",\"username\":\"blah\",\"language_code\":\"en-GB\"},\"chat\":{\"id\":12345,\"first_name\":\"user\",\"username\":\"user\",\"type\":\"private\"},\"date\":1527633135,\"text\":\"hi\"}}"}
 
-	response, err := main.Handler(request)
+	response, err := Handler(request)
 
 	assert.IsType(t, err, nil)
 	expected := "{\"method\":\"sendMessage\",\"chat_id\":0,\"text\":\"Send a sticker to me then I'll be able to add searchable keywords to it.\"}"
@@ -240,13 +238,7 @@ func TestHandler_HandlesKeywordMessageWithRemove(t *testing.T) {
 }
 
 func TestGetAllKeywordsForStickerFileId(t *testing.T) {
-	result := main.GetAllKeywordsForStickerFileId("CAADAgAD8wIAApzW5wrgLgRxhQ_BAgI")
+	result := GetAllKeywordsForStickerFileId("CAADAgAD8wIAApzW5wrgLgRxhQ_BAgI")
 
 	assert.Equal(t, []string{"vader", "thumbs-up", "l27rgjhcmoqomg", "guhympllketj3q"}, result)
-}
-
-func checkErr(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
